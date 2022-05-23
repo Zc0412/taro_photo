0b5ff769e64a7f53e2616cb9b74162d2e5489aff
less <zhangmingcheng0412@gmail.com>
2022-05-23 17:31:55 +0800

    feat:add tabBar

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,6 +1,7 @@
 export default {
   pages: [
     'pages/index/index',
+    'pages/user/index',
     'pages/data/index',
   ],
   window: {
@@ -9,5 +10,24 @@ export default {
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
   },
-  cloud: true
+  cloud: true,
+  tabBar: {
+    // color: '#8A8A8A',
+    selectedColor: '#8331FF',
+    list: [
+      {
+        text: '相册',
+        pagePath: 'pages/index/index',
+        iconPath: './assets/icon/homeIcon.png',
+        selectedIconPath: './assets/icon/selectedHomeIcon.png',
+
+      },
+      {
+        text: '我的',
+        pagePath: 'pages/user/index',
+        iconPath: './assets/icon/userIcon.png',
+        selectedIconPath: './assets/icon/selectedUserIcon.png',
+      }
+    ]
+  }
 }
---
+++ client/src/pages/user/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from "@tarojs/components";
+
+const User = () => {
+  return (
+    <View>
+      user page
+    </View>
+  );
+};
+
+export default User;
