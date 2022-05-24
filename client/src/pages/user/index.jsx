6cc4d31ef93983d586dfaa3fd34a48acf5e49f4f
less <zhangmingcheng0412@gmail.com>
2022-05-24 11:52:07 +0800

    feat:完成用户头像获取

--- client/src/pages/user/index.jsx
+++ client/src/pages/user/index.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
-import { View } from "@tarojs/components";
+import React, { useState } from 'react';
+import { Image, Text, View } from "@tarojs/components";
+import Taro from "@tarojs/taro";
+
+
+import avatar from "../../assets/images/avatar.png"
+import './index.less'
+
 
 const User = () => {
+  // 头像url
+  const [avatarUrl, setAvatarUrl] = useState('')
+  // 昵称
+  const [nickName, setNickName] = useState("")
+  // 用户信息是否获取成功
+  const [hasUserInfo, setHasUserInfo] = useState(false)
+
+  // 获取用户信息
+  const handleGetWXUserInfo = () => {
+    Taro.getUserProfile({
+      desc: '获取用户头像',
+      success: (res) => {
+        setAvatarUrl(res.userInfo.avatarUrl)
+        setNickName(res.userInfo.nickName)
+        setHasUserInfo(true)
+      }
+    })
+  }
+
   return (
-    <View>
-      user page
+    <View className='user-container bg-grid-slate'>
+      {
+        hasUserInfo ? (<View className='user-info'>
+          <Image src={avatarUrl} className='user-info-avatar' />
+          <Text>{nickName}</Text>
+        </View>) : (
+          <View className='user-info' onClick={handleGetWXUserInfo}>
+            <Image src={avatar} className='user-info-avatar' />
+            <Text className='user-info-nick-name'>点击显示微信头像</Text>
+          </View>
+        )
+      }
+
     </View>
   );
 };
