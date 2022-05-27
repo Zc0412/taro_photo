29d0f7320eb602874caef4ece0c5dd1b54da10b4
less <zhangmingcheng0412@gmail.com>
2022-05-27 10:15:51 +0800

    feat:完成feedback

--- client/src/pages/feedback/index.jsx
+++ client/src/pages/feedback/index.jsx
@@ -4,19 +4,42 @@ import Taro from "@tarojs/taro";
 
 
 import './index.less'
+import { db } from "../../untils/db";
 
 const Feedback = () => {
 
   const handleSubmit = (event) => {
-    console.log(event)
-    if (!event.detail.value.feedback) {
+    if (!event?.detail?.value?.feedback) {
       Taro.showToast({
         icon: 'none',
         title: '亲亲，内容不能为空哦~'
       })
       return false
     }
+    // 存储feedback
+    db.collection("photo_album_feedback").add({
+      data: {
+        feedback: event?.detail?.value?.feedback,
+        createAt: db.serverDate()
+      }
+    }).then(() => {
+      // 提交成功返回上一页
+      Taro.navigateBack({
+        delta: 1
+      }).then(() => {
+        Taro.showToast({
+          icon: 'success',
+          title: '反馈成功'
+        })
+      })
+    }).catch(() => {
+      Taro.showToast({
+        icon: 'error',
+        title: '反馈失败，请重试!'
+      })
+    })
   }
+
   return (
     <View className='feedback-container'>
       <Form onSubmit={handleSubmit}>
@@ -34,7 +57,7 @@ const Feedback = () => {
           </View>
         </View>
 
-        <Button formType='submit' type='warn'>提交</Button>
+        <Button formType='submit' type='warn' className='feedback-submit'>提交</Button>
         <Button formType='reset' type='default'>重置</Button>
 
       </Form>
